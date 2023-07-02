0b70c464121f3e47fab9e439feffd64258fdf564
SheffyBansal <70812725+SheffyBansal@users.noreply.github.com>
2023-07-02 21:27:09 +0530

    Add files via upload

---
+++ src/Components/CheckoutProduct.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './checkoutproduct.css';
+import { useStateValue } from '../StateProvider';
+
+function CheckoutProduct({id,title,image,price,rating}) {
+    const[{basket},dispatch] = useStateValue();
+
+    const removeFromBasket= () =>{
+     dispatch(
+        {
+            action: 'REMOVE_FROM_BASKET',
+            id: id
+        }
+        
+     );
+    }
+
+  return (
+    <div className="checkoutProduct">
+      <img className='checkoutProduct__image' src={image} alt="product_image"/>
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+            <small>₹</small>
+            <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+            {
+                Array(rating).fill().map((_)=>{
+                     return <p>⭐</p>
+                })
+            }
+        </div>
+        <button onClick={()=>removeFromBasket()}>Remove from Basket</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default CheckoutProduct
---
+++ src/Components/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVGHmZ0pICwG8MDjOVU5OmU5KdN0i8Jtg",
+    authDomain: "clone-1663f.firebaseapp.com",
+    projectId: "clone-1663f",
+    storageBucket: "clone-1663f.appspot.com",
+    messagingSenderId: "383945416586",
+    appId: "1:383945416586:web:ffc243f37cbcea366bb273",
+    measurementId: "G-EHSZWVSHXG"
+  };
